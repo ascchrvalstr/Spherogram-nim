80fd63087574ec0ed7e48b0cedbace8f2e5a706a
ascchrvalstr <12978630+ascchrvalstr@users.noreply.github.com>
2025-07-14 13:43:23 +0800

    implement seifert.admissible_moves

--- src/seifert.nim
+++ src/seifert.nim
@@ -5,6 +5,7 @@ Code written by Malik Obeidin.
 """
 import std/strformat
 import std/sugar
+import std/enumerate
 
 import links
 
@@ -35,3 +36,26 @@ proc seifert_circles*[T](link: Link[T]): seq[seq[(int, int)]] =
                         cur_s = (op_s - link.signs[op_c] + 4) mod 4
                 circles.add(circle)
     return circles
+
+proc admissible_moves*[T](link: Link[T]): (seq[((int, int), (int, int))], seq[(int, int)]) =
+    var circles = seifert_circles(link)
+    let num_crossings = link.crossings.len
+    var cs_to_seifert_circle = newSeq[array[0..3, int]](num_crossings)
+    for (circle_index, circle) in enumerate(circles):
+        for (strand_c, strand_s) in circle:
+            cs_to_seifert_circle[strand_c][strand_s] = circle_index
+            let (op_c, op_s) = link.opposite_strand((strand_c, strand_s))
+            cs_to_seifert_circle[op_c][op_s] = circle_index
+    # echo cs_to_seifert_circle
+    var pairs = newSeq[((int, int), (int, int))]()
+    var seifert_circle_pairs = newSeq[(int, int)]()
+    for face in link.faces():
+        for i in 0 ..< face.len-1:
+            for j in i+1 ..< face.len:
+                let (cs1, cs2) = (face[i], face[j])
+                let (circle1, circle2) = (cs_to_seifert_circle[cs1.crossing][cs1.strand_index],
+                                          cs_to_seifert_circle[cs2.crossing][cs2.strand_index])
+                if circle1 != circle2:
+                    pairs.add((cs1.to_pair(), cs2.to_pair()))
+                    seifert_circle_pairs.add((circle1, circle2))
+    return (pairs, seifert_circle_pairs)
